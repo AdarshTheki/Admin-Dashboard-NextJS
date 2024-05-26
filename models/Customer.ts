2e5154583940132c0bbda4965a1fd0a62f44c8b5
Adarsh <adarshverma549@gmail.com>
2024-05-26 14:53:34 +0530

    adding a models of collection, customer etc...

---
+++ models/Customer.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const customerSchema = new mongoose.Schema(
+    {
+        clerkId: String,
+        name: String,
+        email: String,
+        orders: {
+            type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
+        },
+    },
+    { timestamps: true }
+);
+
+const Customer = mongoose.models.Customer || mongoose.model('Customer', customerSchema);
+
+export default Customer;
---
+++ models/Order.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema(
+    {
+        customerClerkId: String,
+        products: [
+            {
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'Product',
+                },
+                color: String,
+                size: String,
+                quantity: Number,
+            },
+        ],
+        shippingAddress: {
+            street: String,
+            city: String,
+            state: String,
+            postalCode: String,
+            country: String,
+        },
+        shippingRate: String,
+        totalAmount: Number,
+    },
+    { timestamps: true }
+);
+
+const Order = mongoose.models.Order || mongoose.model('Order', orderSchema);
+
+export default Order;
