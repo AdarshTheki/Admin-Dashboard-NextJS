b3f43c27c1cfd71bfb0d8ee9adc25985bbe3ffa3
Adarsh <adarshverma549@gmail.com>
2024-05-26 21:48:48 +0530

    Updated clerkMiddleware configuration and removed auth callback

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,15 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up', '/api/:path*']);
 
-export default clerkMiddleware(
-    (auth, request) => {
-        if (!isPublicRoute(request)) {
-            auth().protect();
-        }
-    },
-    {
-        authorizedParties: ['http://localhost:3000', 'http://localhost:4000'],
-        clockSkewInMs: 1000 * 60 * 60 * 24 * 2,
-    }
-);
+export default clerkMiddleware({
+    authorizedParties: [
+        'http://localhost:3000',
+        'http://localhost:4000',
+        'https://admin-dashboard-next-js-xi.vercel.app',
+        'https://ecommerce-store-next-js-five.vercel.app',
+    ],
+    clockSkewInMs: 1000 * 60 * 60 * 24 * 2,
+});
 
 export const config = {
     matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
